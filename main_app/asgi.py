29272b11b054b78e8c4b72d21fa4b6a782ebcf22
Utsav-56 <utsavpokhrel200@gmail.com>
2026-02-19 23:33:11 +0545

    FIxed the asgi load issue

--- main_app/asgi.py
+++ main_app/asgi.py
@@ -1,28 +1,23 @@
-"""
-ASGI config for main_app project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+
+# 1. Set settings first
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main_app.settings")
+
+# 2. Initialize the Django ASGI application early.
+# This "wakes up" Django so models/settings can be accessed.
+django_asgi_app = get_asgi_application()
+
+# 3. NOW you can import your custom code
 from channels.routing import ProtocolTypeRouter, URLRouter
 import main_app.routing
 from main_app.middleware import WebSocketJWTAuthMiddleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_app.settings')
-
-django_asgi_app = get_asgi_application()
-
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": WebSocketJWTAuthMiddleware(
-        URLRouter(
-            main_app.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": WebSocketJWTAuthMiddleware(
+            URLRouter(main_app.routing.websocket_urlpatterns)
+        ),
+    }
+)
